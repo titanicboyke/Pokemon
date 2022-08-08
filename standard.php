e68f134a608323267415424c0c362181a10e15c2
Dave <titanicboyke@telenet.be>
2022-08-08 08:28:14 +0200

    Update standard.php

--- standard.php
+++ standard.php
@@ -1,1 +1,1 @@
-<center><img src="https://raw.githubusercontent.com/titanicboyke/siteicons/master/Trainer.png" alt=""><br />Welcome to this Map, <br />we wish you lots of fun!<hr>This MAP is limited, you get full access after a monthly payment of 3 euros. For more info join our <a href="$discordUrl">Discord</a>.</center>
+<center><img src="https://raw.githubusercontent.com/titanicboyke/siteicons/master/Trainer.png" alt=""></center>
